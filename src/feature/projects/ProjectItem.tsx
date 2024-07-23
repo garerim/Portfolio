310ffaae3c8610013e19126b962ed43265477b92
Matheo <garerimath73660@gmail.com>
2024-07-23 22:48:31 +0200

    Add new project

--- src/feature/projects/ProjectItem.tsx
+++ src/feature/projects/ProjectItem.tsx
@@ -2,11 +2,12 @@ import { ExternalLink } from 'lucide-react'
 import BadgeTech from '../BadgeTech'
 import { RevealWrapper } from 'next-reveal'
 import Image from 'next/image'
+import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from '@/components/ui/carousel'
 
 type ProjectItemProps = {
   name: string,
   description: string,
-  image: string,
+  image: string[],
   url: string,
   techs: {
     name: string
@@ -17,22 +18,37 @@ type ProjectItemProps = {
 export default function ProjectItem({ project, index }: { project: ProjectItemProps, index: number }) {
 
   return (
-    <RevealWrapper rotate={{ x: 12, y: 40, z: 0 }} origin='bottom' delay={200 + (index*100)} duration={1000} distance='100px' reset={false} viewOffset={{ top: 25, right: 0, bottom: 10, left: 5 }}>
-      <div className='border-4 h-[440px] bg-card rounded-lg w-fit p-3 min-w-xs max-w-lg max-sm:h-auto flex flex-col'>
-
+    // <RevealWrapper rotate={{ x: 12, y: 40, z: 0 }} origin='bottom' delay={200 + (index * 100)} duration={1000} distance='10px' reset={false} viewOffset={{ top: 25, right: 0, bottom: 10, left: 5 }}>
+    <div className='border-4 bg-card rounded-lg w-fit p-3 min-w-xs max-w-lg max-sm:h-auto flex flex-col'>
+      {/* aspect-square h-[440px] */}
+      {project.image.length > 1 ? (
+        <Carousel className="max-h-64 w-full">
+          <CarouselContent>
+            {project.image.map((img, index) => (
+              <CarouselItem key={index}>
+                <img src={`/images/${img}.png`} alt={"Photo " + img} className='rounded-md max-h-64 w-full object-cover' />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className='left-2 top-1/2' />
+          <CarouselNext className='right-2 top-1/2' />
+        </Carousel>
+      ) : (
         <img src={`/images/${project.image}.png`} alt={"Photo " + project.image} className='rounded-md max-h-64 w-full object-cover' />
+      )}
 
-        <h3 className='font-bold text-lg flex gap-2 justify-center items-center'>{project.name} {project.url && <a href={project.url}><ExternalLink className='w-5 h-5' /></a>}</h3>
+      <h3 className='font-bold text-lg flex gap-2 justify-center items-center'>{project.name} {project.url && <a href={project.url}><ExternalLink className='w-5 h-5' /></a>}</h3>
 
-        <p className='text-xs'>{project.description}</p>
+      <p className='text-xs'>{project.description}</p>
 
-        <div className='flex gap-2 flex-wrap justify-center mt-auto'>
-          {project.techs.map((tech, index) => (
-            <BadgeTech key={index} svg={tech.svg} name={tech.name} />
-          ))}
-        </div>
-
+      <div className='flex gap-2 flex-wrap justify-center mt-auto pt-2'>
+        {project.techs.map((tech, index) => (
+          <BadgeTech key={index} svg={tech.svg} name={tech.name} />
+        ))}
       </div>
-    </RevealWrapper>
+
+    </div>
+    // </RevealWrapper >
   )
+
 }
--- src/feature/projects/Projects.tsx
+++ src/feature/projects/Projects.tsx
@@ -2,6 +2,8 @@
 
 import { GetTranslate } from '@/app/[locale]/getTranslate';
 import ProjectItem from './ProjectItem';
+import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from '@/components/ui/carousel';
+import { RevealWrapper } from 'next-reveal';
 
 export default function ProjectsPage() {
 
@@ -9,7 +11,7 @@ export default function ProjectsPage() {
     {
       "name": "Githread",
       "description": GetTranslate("ProjectsPage", 'projects.projects-1'),
-      "image": "githread-1",
+      "image": ["githread-1"],
       "url": "https://github.com/garerim/Githread",
       "techs": [
         {
@@ -33,7 +35,7 @@ export default function ProjectsPage() {
     {
       "name": "Clone Twitter",
       "description": GetTranslate("ProjectsPage", 'projects.projects-2'),
-      "image": "twitter-1",
+      "image": ["twitter-1"],
       "url": "https://github.com/garerim/Projet-Clone-Twitter-React",
       "techs": [
         {
@@ -57,7 +59,7 @@ export default function ProjectsPage() {
     {
       "name": "Clone Troc.com",
       "description": GetTranslate("ProjectsPage", 'projects.projects-3'),
-      "image": "troc-1",
+      "image": ["troc-1"],
       "url": "https://github.com/garerim/SAE401-VueJS",
       "techs": [
         {
@@ -81,7 +83,7 @@ export default function ProjectsPage() {
     {
       "name": "Pulse Chat",
       "description": GetTranslate("ProjectsPage", 'projects.projects-4'),
-      "image": "pulsechat",
+      "image": ["pulsechat"],
       "url": "https://pulsechat-poll.vercel.app/home",
       "techs": [
         {
@@ -101,19 +103,67 @@ export default function ProjectsPage() {
           "svg": "tailwind"
         }
       ]
-    }
+    },
+    {
+      "name": "Miro Clone",
+      "description": GetTranslate("ProjectsPage", 'projects.projects-5'),
+      "image": ["miro-1", "miro-2"],
+      "url": "https://miro-clone-sand.vercel.app",
+      "techs": [
+        {
+          "name": "NextJS",
+          "svg": "nextjs"
+        },
+        {
+          "name": "TypeScript",
+          "svg": "typescript"
+        },
+        {
+          "name": "TailwindCSS",
+          "svg": "tailwind"
+        },
+        {
+          "name": "Convex",
+          "svg": "convex"
+        },
+        {
+          "name": "Liveblocks",
+          "svg": "liveblocks"
+        },
+      ]
+    },
   ]
 
 
 
   return (
-    <div id='projects' className='min-h-screen relative pt-16'>
+    <div id='projects' className='h-auto pb-32 relative pt-16'>
       <h1 className='text-center text-4xl font-bold mb-4'>{GetTranslate('ProjectsPage', 'title')}</h1>
-      <div className='flex justify-center items-center gap-2 flex-wrap px-2'>
+      {/* <div className='flex justify-center items-center gap-2 flex-wrap px-2'>
         {data.map((project, index) => (
           <ProjectItem key={index} project={project} index={index} />
-        ))}
-      </div>
+          ))}
+          </div> */}
+      <RevealWrapper rotate={{ x: 0, y: 40, z: 0 }} origin='bottom' delay={200} duration={1000} distance='100px' reset={false} viewOffset={{ top: 25, right: 0, bottom: 100, left: 5 }}>
+        <Carousel
+          opts={{
+            align: "start",
+          }}
+          className="max-w-[80%] mx-auto"
+        >
+          <CarouselContent>
+            {data.map((project, index) => (
+              <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                <div className="p-1">
+                  <ProjectItem project={project} index={index} />
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </RevealWrapper>
     </div>
   )
 }
